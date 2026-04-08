d17e4b4d966fd255a971ec668c72f77238a184bf
Mouhib Yassine <87534925+none159@users.noreply.github.com>
2026-04-09 01:42:52 +0200

    Add: plus-one - submission-3

---
+++ Data Structures & Algorithms/plus-one/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+        while i >= 0:
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                i -= 1 
+                continue
+        digits.append(1) 
+        return digits[::-1]
+
+# My brute force way
