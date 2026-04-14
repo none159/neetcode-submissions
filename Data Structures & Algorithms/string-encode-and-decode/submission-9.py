c89b71d0e02f3e9ac0ea186303ca41ca57c1d1d7
Yassine Mouhib <noneenone019@gmail.com>
2026-04-14 17:19:11 +0100

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,18 @@
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += s + ":;"
+        return res
+
+        return res
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        res = s.split(":;")
+        res = res[:-1]
+        return res
+
+# other way of solving the problem
