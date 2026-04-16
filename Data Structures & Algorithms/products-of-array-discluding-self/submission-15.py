6d68afe49ceb56970a16cb3fec1cb81fe7ed1065
Yassine Mouhib <noneenone019@gmail.com>
2026-04-16 23:22:37 +0100

    Add: products-of-array-discluding-self - submission-15

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        i = 0
+        result = []
+        zero_mult = nonzero_mult = 1
+        for n in nums:
+            if n!=0:
+                nonzero_mult *= n
+            zero_mult *= n
+        
+        if nums.count(0) > 1:
+            return [0] * len(nums)
+        for n in nums:
+            if 0 in nums:
+                result.append(zero_mult//n if n != 0 else nonzero_mult)
+            else:
+                result.append(nonzero_mult//n)
+        return result
+
+# my other attempt with a help => complexity:o(n), space: o(n) but with restricted division
